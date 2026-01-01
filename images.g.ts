9726e97a461aa0140c8626be96618cbcb3661a81
julianmainaguez <julian.mainaguez@gmail.com>
2026-01-01 14:36:00 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "toad":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 1 2 1 2 1 . . . . . 
+. . . . . 1 2 1 2 2 2 1 . . . . 
+. . . . . . d f d f d . . . . . 
+. . . . . . d d d d d . . . . . 
+. . . . . . d d d d d . . . . . 
+. . . . . . 2 1 1 1 2 . . . . . 
+. . . . . d 2 1 1 1 2 d . . . . 
+f f f . d . 2 1 1 1 2 . d . . . 
+f f f f f . 2 1 1 1 2 . d . . . 
+f f f . e e b b . b b e e . . . 
+. . . . e e e e . e e e e . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,3 @@
+let toad = sprites.create(assets.image`toad`, SpriteKind.Player)
+controller.moveSprite(toad)
+scene.setBackgroundColor(9)
